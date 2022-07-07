8b339824f628b4155ede73097e2a25660bb7461e
muhammdrifq <muhammad_087@smkassalaambandung.sch.id>
2022-07-07 14:18:37 +0700

    title Change

--- Guru/index.php
+++ Guru/index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <title>PENDATAAN GURU | Index</title>
+    <title>PENDATAAN GURU | Landing Page</title>
   </head>
   <body>
         <!-- Navbar -->
